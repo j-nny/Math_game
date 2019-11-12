d543ef5d9f82f0ad5fb2f4ecf96bc2c36ecd1c34
Jenny Nguy <j.nguy809@gmail.com>
2019-11-11 22:46:51 -0500

    add score change logic

--- main.rb
+++ main.rb
@@ -1,18 +1,47 @@
-def math_question
+class Player
+  attr_accessor :player, :score
 
-  number1 = 1
-  number2 = 2
-  puts "What does #{number1} plus #{number2} equal?"
-
-  print "> "
-  answer = $stdin.gets.chomp
-
-  if (answer.to_i == (number1 + number2))
-    puts 'yes'
-  else
-    puts 'no'
+  def initialize(player, score)
+    @player = player
+    @score = score
   end
 
 end
 
-math_question
+
+# Asks a math question
+def math_question(player)
+
+  number1 = rand 10
+  number2 = rand 10
+  puts "What does #{number1} plus #{number2} equal?"
+  
+  print "> "
+  answer = $stdin.gets.chomp
+  
+  if (answer.to_i == (number1 + number2))
+    puts "#{player.player}: YES! You are correct"
+    puts "P1: #{$p1.score}/3 vs P2: #{$p2.score}/3"
+  else
+    puts "#{player.player}: Seriously? No!"
+    player.score -= 1
+    puts "P1: #{$p1.score}/3 vs P2: #{$p2.score}/3"
+  end
+
+end
+
+def game_board
+  
+  $p1 = Player.new("Player 1", 2)
+  $p2 = Player.new("Player 2", 3)
+  
+  turn = $p1
+
+  math_question(turn)
+
+  # prompt next player
+  turn == $p1 ? turn = $p2 : turn = $p1
+
+end
+
+game_board
